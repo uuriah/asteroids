121a275426dac552b246ffd22033280a5a8ca67a
uuriah <132951774+uuriah@users.noreply.github.com>
2025-12-20 13:22:20 -0800

    Create game loop. Comment out the print statements they were causing the pygame window to notdisplay

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import pygame
+import pygame as pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
-    print("Starting Asteroids with pygame version:", pygame.version.ver)
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # print("Starting Asteroids with pygame version:", pygame.version.ver)
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
+    while True:
+        log_state()
+        # stop program if user closes window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
